72b9e9441bbc339bdb640285c5af575ba067d868
wangzongtao <wangzt2@tiens.com>
2021-01-27 17:51:44 +0800

    初次提交

---
+++ src/main/java/com/ming/Application.java
@@ -0,0 +1,19 @@
+package com.ming;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.ming"})
+@Log4j2
+public class Application implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("服务启动成功！");
+    }
+}
